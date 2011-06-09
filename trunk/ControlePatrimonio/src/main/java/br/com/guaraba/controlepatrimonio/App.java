3bdd185d6a89975ca1f6ad8be8d43f1bfa915eaf
marcelogomesrp@gmail.com <marcelogomesrp@gmail.com@e6505ccc-68dd-c18c-6884-1b1c3a55c207>
2011-06-09 14:08:51 +0000

    Iniciado o projeto

---
+++ trunk/ControlePatrimonio/src/main/java/br/com/guaraba/controlepatrimonio/App.java
@@ -0,0 +1,38 @@
+package br.com.guaraba.controlepatrimonio;
+
+import br.com.guaraba.controlepatrimonio.patrimonio.Patrimonio;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Patrimonio pt = new Patrimonio();
+        pt.setNome("teste4");
+        App.persist(pt);
+
+        //org.slf4j.impl.StaticLoggerBinder
+
+    }
+
+    public static void persist(Object object) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PatrimonioPU");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+}
