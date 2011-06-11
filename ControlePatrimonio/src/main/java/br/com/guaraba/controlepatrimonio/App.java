cfe12823373e07f96d2527b99b9bd18ef0f291bd
marcelogomesrp@gmail.com <marcelogomesrp@gmail.com@e6505ccc-68dd-c18c-6884-1b1c3a55c207>
2011-06-11 11:52:20 +0000

    Add suporte ao spring... mas tem muita coisa a mais no pom.xml

--- ControlePatrimonio/src/main/java/br/com/guaraba/controlepatrimonio/App.java
+++ ControlePatrimonio/src/main/java/br/com/guaraba/controlepatrimonio/App.java
@@ -1,9 +1,11 @@
 package br.com.guaraba.controlepatrimonio;
 
+import br.com.guaraba.controlepatrimonio.instituicao.Instituicao;
 import br.com.guaraba.controlepatrimonio.patrimonio.Patrimonio;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.JOptionPane;
 
 /**
  * Hello world!
@@ -14,9 +16,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Instituicao hemocentro = new Instituicao("Hemocentro de Ribeirão Preto", "HEMO");
+        Instituicao usp = new Instituicao("Universidade de São Paulo", "USP");
+        Instituicao hc = new Instituicao("Hospital das clinicas de Ribeirão Preto", "HCRP");
+        App.persist(hemocentro);
+        App.persist(usp);
+        App.persist(hc);
         Patrimonio pt = new Patrimonio();
         pt.setNome("teste5");
-        App.persist(pt);
+        //App.persist(pt);
 
         //org.slf4j.impl.StaticLoggerBinder
 
@@ -31,6 +39,7 @@ public class App
             em.getTransaction().commit();
         } catch (Exception e) {
             em.getTransaction().rollback();
+            JOptionPane.showMessageDialog(null, "Erro ao persistir objeto " + e.getMessage());
         } finally {
             em.close();
         }
---
+++ ControlePatrimonio/src/main/java/br/com/guaraba/controlepatrimonio/instituicao/InstituicaoDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.guaraba.controlepatrimonio.instituicao;
+
+import br.com.guaraba.controlepatrimonio.dao.DaoGenerico;
+
+/**
+ *
+ * @author marcelo
+ */
+public interface InstituicaoDao extends DaoGenerico<Instituicao, Long>{
+
+}
