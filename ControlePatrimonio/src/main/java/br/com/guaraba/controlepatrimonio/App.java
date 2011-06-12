c7a472e83be859f40b07b48a8a93dfd07cb40784
marcelogomesrp@gmail.com <marcelogomesrp@gmail.com@e6505ccc-68dd-c18c-6884-1b1c3a55c207>
2011-06-12 15:30:53 +0000

    ta dando erro no spring

--- ControlePatrimonio/src/main/java/br/com/guaraba/controlepatrimonio/App.java
+++ ControlePatrimonio/src/main/java/br/com/guaraba/controlepatrimonio/App.java
@@ -1,11 +1,19 @@
 package br.com.guaraba.controlepatrimonio;
 
 import br.com.guaraba.controlepatrimonio.instituicao.Instituicao;
+import br.com.guaraba.controlepatrimonio.instituicao.InstituicaoDao;
 import br.com.guaraba.controlepatrimonio.patrimonio.Patrimonio;
+import java.io.File;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.swing.JOptionPane;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 /**
  * Hello world!
@@ -13,8 +21,13 @@ import javax.swing.JOptionPane;
  */
 public class App 
 {
+    static Logger logger = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
+        
+        PropertyConfigurator.configure("log4j.properties");
+        logger.info("?Inicializando...?");
         System.out.println( "Hello World!" );
         Instituicao hemocentro = new Instituicao("Hemocentro de Ribeirão Preto", "HEMO");
         Instituicao usp = new Instituicao("Universidade de São Paulo", "USP");
@@ -28,6 +41,53 @@ public class App
 
         //org.slf4j.impl.StaticLoggerBinder
 
+
+
+//        File file = new File("applicationContext.xml");
+//        FileSystemResource fileResource = new FileSystemResource( file );
+//        XmlBeanFactory mFactory = new XmlBeanFactory( fileResource );
+//
+
+        
+        
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        ApplicationContext applicationContext   = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //ApplicationContext applicationContext = new ClassPathXmlApplicationContext(file.getAbsolutePath());
+
+
+
+
+        //InstituicaoDao instituicaoDao = (InstituicaoDao) mFactory.getBean("instituicaoDao");
+        InstituicaoDao instituicaoDao = (InstituicaoDao) applicationContext.getBean("instituicaoDao");
+
+        Instituicao spring = new Instituicao("spring", "spring");
+        //EntityManagerFactory emf = (EntityManagerFactory) mFactory.getBean("entityManagerFactory");
+
+
+        instituicaoDao.salvar(spring);
+
+       // System.out.println("Path: " + fileResource.getPath());
+        
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        InstituicaoDao instituicaoDao = (InstituicaoDao) context.getBean("instituicaoDao");
+        //org.apache.commons.logging.
+
+        
+    }
+
+    public static void persist(Object object, EntityManagerFactory emf) {        
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            JOptionPane.showMessageDialog(null, "Erro ao persistir objeto " + e.getMessage());
+        } finally {
+            em.close();
+        }
     }
 
     public static void persist(Object object) {
---
+++ ControlePatrimonio/src/main/java/br/com/guaraba/controlepatrimonio/instituicao/InstituicaoDaoImp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.guaraba.controlepatrimonio.instituicao;
+
+import br.com.guaraba.controlepatrimonio.dao.imp.DaoGenericoImp;
+
+/**
+ *
+ * @author marcelo
+ */
+public class InstituicaoDaoImp extends DaoGenericoImp<Instituicao, Long> implements InstituicaoDao  {
+
+}
